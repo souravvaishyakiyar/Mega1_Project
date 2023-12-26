e48537bb1f5a5b08455d81df84f3add3d407de88
souravvaishyakiyar <souravvaishyakiyar@gmail.com>
2023-12-26 18:07:33 +0530

    Complete

---
+++ Blog1_Project/src/conf/conf.js
@@ -0,0 +1,17 @@
+const conf ={
+    // appwriteUrl:String(import.meta.env.VITE_APPWRITE_URL),
+    // appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    // appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    // appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    // appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+
+    appwriteUrl:"https://cloud.appwrite.io/v1",
+    appwriteProjectId: "65897c80a4a9b33c0d58",
+    appwriteDatabaseId: "65897d497196d5b18dcc",
+    appwriteCollectionId: "65897d8b39c3388c5795",
+    appwriteBucketId: "65897f8eaa914ab877d5"
+    
+
+}
+
+export default conf
